dfd539e039241abe47b6b957b1ab05c13129d08b
EldarionDev <janek.schultes.privat@gmail.com>
2021-04-01 22:50:28 +0200

    Bug fix; Reserve dl by BIOS

--- boot.asm
+++ boot.asm
@@ -6,21 +6,26 @@ section .boot_code
 global _start
 _start:
 	mov sp, 0x7000
+	push dx ;Reserve dl
+
 	call setup_print
 	push '!'
 	call print_char
+	pop ax
 	push 't'
 	call print_char
-	
+	pop ax
+
+	pop dx ;dl has value assigned by BIOS
 	mov ah, 2 ;Read disk 
 	mov al, 1 ;Read 1 sector 
 	mov ch, 0 ;Read cylinder 0 
 	mov cl, 2 ;Read sector 2 
 	mov dh, 0 ;Read head 0 
-	mov dl, 0 ;Read disk with stage0 bootloader on 
 	mov bx, 0x7e00
 	mov es, bx
 	mov bx, 0
+	xchg bx, bx
 	int 0x13
 
 	lea bx, boot_string
